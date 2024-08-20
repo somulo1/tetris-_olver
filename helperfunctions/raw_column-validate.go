5aa6be9f53ef830c87b5ab325c83ddabb33b8ad3
somulo <mcomulosammy37@gmail.com>
2024-08-20 15:54:02 +0300

    chore: check for hash and dots

--- helperfunctions/raw_column-validate.go
+++ helperfunctions/raw_column-validate.go
@@ -3,21 +3,41 @@
 package helperfunctions
 
 import (
+	"fmt"
 	"strings"
 )
 
 // validate the tetromin arrangement
 func ValidateTetrominoFormat(tetromino string) bool {
 	lines := strings.Split(strings.TrimSpace(tetromino), "\n")
+	fmt.Println(lines)
 	if len(lines) != 4 {
 		return false
 	}
+	var countDots int
+	var countHash int
+	// check for every line
 
-	//check for every line
-	for _, line := range lines {
+	for i, line := range lines {
+		//fmt.Println(line)
 		if len(line) != 4 {
 			return false
 		}
+		// for _ , char := range line {
+		// 	fmt.Print(char)
+		// }
+		if string(line[i]) == "." {
+			countDots++
+		}
+		if string(line[i]) == "#" {
+			countHash++
+		}
+
+	}
+	if countDots+countHash != 16 || countDots != 12 || countHash != 4 {
+		fmt.Errorf("\nERROR1.1")
+		return false
+
 	}
 	return true
 }
