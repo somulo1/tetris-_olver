08ec4d7cf60e9b7c3272903f0d646d720e3d5222
somulo <mcomulosammy37@gmail.com>
2024-08-20 15:54:48 +0300

    chore apply func validate tetromino

--- main.go
+++ main.go
@@ -8,49 +8,44 @@ import (
 )
 
 func main() {
-	// Limit CLI arguments to 1 (assuming the file path is the only argument)
+	// Limit CLI arguments to 2  argument
 	if len(os.Args) != 2 {
 		fmt.Println("Usage: go run . <something.txt>")
 		return
 	}
 	filePath := os.Args[1]
 
-	// 1. Check if file is empty
+	//limit file type to a text file
+	if !helperfunctions.Check_extension(filePath) {
+		fmt.Println("Only provide a .txt file")
+		return
+	}
+	//  Check if file is empty
 	if err := helperfunctions.CheckIfFileIsEmpty(filePath); err != nil {
-		fmt.Println(err)
+		fmt.Println("ERROR")
 		return
 	}
 
-	// 2. Read file content
+	//  Read file content
 	content, err := helperfunctions.ReadFile(filePath)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ERROR")
 		return
 	}
 
-	// 3. Parse tetrominoes
-	tetrominoes, err := helperfunctions.CheckTetris(content)
-	if err != nil {
-		fmt.Println("Error parsing tetrominoes")
-		return
-	}
+	//  Parse tetrominoes
+	tetrominoes := helperfunctions.CheckTetris(content)
 
-	// 4. Validate tetromino format
+	// Validate tetromino format
 	for _, tetromino := range tetrominoes {
 		if !helperfunctions.ValidateTetrominoFormat(tetromino) {
-			fmt.Println("ERROR: Invalid tetromino format")
+			fmt.Println("ERROR1")
 			return
 		}
+		
 	}
+	// trim the tetromino
+	// placing the tetris 
 
-	// Example board dimensions
-	boardWidth := 10
-	boardHeight := 10
 
-	// 5. Execute board operations for each tetromino and print results
-	for _, tetromino := range tetrominoes {
-		// Call ExecuteBoardOperations with example coordinates (0, 0) and print the result
-		boardRepresentation := helperfunctions.ExecuteBoardOperations(boardWidth, boardHeight, *tetrominoes, 0, 0)
-		fmt.Println(boardRepresentation)
-	}
 }
