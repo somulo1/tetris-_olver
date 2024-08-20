ec18a1b642dcadfcde6322b5a0afa652e66244ef
somulo <mcomulosammy37@gmail.com>
2024-08-20 14:29:47 +0300

    fix: check raw

--- helperfunctions/raw_column-validate.go
+++ helperfunctions/raw_column-validate.go
@@ -1,22 +1,23 @@
-//the tetrominos should have a total of four row and four columns
+// the tetrominos should have a total of four row and four columns
 // the number of "#" in each cube must be equal to 4
 package helperfunctions
 
 import (
 	"strings"
 )
-//validate the tetromin arrangement
-func ValidateTetrominoFormat(filename string) bool {
-	lines := strings.Split(strings.TrimSpace(filename), "\n")
+
+// validate the tetromin arrangement
+func ValidateTetrominoFormat(tetromino string) bool {
+	lines := strings.Split(strings.TrimSpace(tetromino), "\n")
 	if len(lines) != 4 {
 		return false
 	}
-	width := len(lines[0])
+
+	//check for every line
 	for _, line := range lines {
-		if len(line) != 4 && width != 4 {
+		if len(line) != 4 {
 			return false
 		}
 	}
 	return true
-	
 }
