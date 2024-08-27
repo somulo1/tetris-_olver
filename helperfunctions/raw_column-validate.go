c762462c22fdf3b6e07f09de561463ac81c6e092
somulo <mcomulosammy37@gmail.com>
2024-08-27 09:53:41 +0300

    fix: fix import functions

--- helperfunctions/raw_column-validate.go
+++ helperfunctions/raw_column-validate.go
@@ -1,43 +1,27 @@
-// the tetrominos should have a total of four row and four columns
-// the number of "#" in each cube must be equal to 4
 package helperfunctions
 
-import (
-	"fmt"
-	"strings"
-)
+import "Tetris-optimizer/helperfunctions/validity"
 
-// validate the tetromin arrangement
-func ValidateTetrominoFormat(tetromino string) bool {
-	lines := strings.Split(strings.TrimSpace(tetromino), "\n")
-	fmt.Println(lines)
-	if len(lines) != 4 {
+// ValidateTetromino validates a tetromino by calling the necessary functions and checks.
+func ValidateTetromino(tet []string) bool {
+	// Trim the tetromino to remove unnecessary columns and rows
+	trimmedTetromino := TetroTrim(tet)
+
+	// Check if the trimmed tetromino is a valid 4x4 representation
+	if !validity.FourByFour(trimmedTetromino) {
 		return false
 	}
-	var countDots int
-	var countHash int
-	// check for every line
 
-	for i, line := range lines {
-		//fmt.Println(line)
-		if len(line) != 4 {
-			return false
-		}
-		// for _ , char := range line {
-		// 	fmt.Print(char)
-		// }
-		if string(line[i]) == "." {
-			countDots++
-		}
-		if string(line[i]) == "#" {
-			countHash++
-		}
-
-	}
-	if countDots+countHash != 16 || countDots != 12 || countHash != 4 {
-		fmt.Errorf("\nERROR1.1")
+	// Check if the trimmed tetromino contains exactly four hashes
+	if !validity.FourHashes(trimmedTetromino) {
 		return false
-
 	}
+
+	// Check if the hashes in the tetromino are connected properly
+	if !validity.ValidConnections(trimmedTetromino) {
+		return false
+	}
+
+	// If all checks pass, return true
 	return true
 }
