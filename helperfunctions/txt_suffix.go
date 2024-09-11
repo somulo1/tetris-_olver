6d457da9db57fa6d4a72d050924eeb9f2e8e77ec
somulo <mcomulosammy37@gmail.com>
2024-09-11 14:27:23 +0300

    chore: delete reused files and functions

--- helperfunctions/txt_suffix.go
+++
@@ -1,9 +0,0 @@
-package helperfunctions
-
-func hasSuffix(s, suffix string) bool {
-	return s[len(s)-4:] == suffix
-}
-
-func isValidFile(fileName string) bool {
-	return hasSuffix(fileName, ".txt")
-}
